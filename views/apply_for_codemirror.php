a286805fb7c5415b88cc1ad4aa9896d0187e7cf3
rewish <rewish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-18 14:12:26 +0000

    WP Emmet 0.2.3 Released
    git-svn-id: https://plugins.svn.wordpress.org/wp-emmet/trunk@742780 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/apply_for_codemirror.php
+++ views/apply_for_codemirror.php
@@ -1,5 +1,20 @@
+<style>
+.CodeMirror {
+<?php echo $this->Options->get('codemirror_style') . PHP_EOL; ?>
+}
+</style>
 <script>
-!function($) {
+<?php if ($this->Options->get('override_shortcuts')): ?>
+var emmetKeymap = {
+<?php foreach ($shortcuts as $label => $keystroke): ?>
+	'<?php echo $keystroke; ?>': '<?php echo str_replace(array(' ', '/', '.'), array('_', '_', ''), strtolower($label)); ?>',
+<?php endforeach; ?>
+	Tab: 'expand_abbreviation_with_tab',
+	Enter: 'insert_formatted_line_break_only'
+};
+<?php endif; ?>
+
+jQuery(function($) {
 	var options = $.extend(<?php echo $this->Options->toJSON('codemirror'); ?>, {
 			profile: '<?php echo $this->Options->get('profile'); ?>'
 		}),
@@ -11,27 +26,28 @@
 			json: 'application/json'
 		};
 
-<?php if ($this->Options->get('override_shortcuts')): ?>
-	window.emmetKeymap = {
-<?php foreach ($shortcuts as $label => $keystroke): ?>
-		'<?php echo $keystroke; ?>': '<?php echo str_replace(array(' ', '/', '.'), array('_', '_', ''), strtolower($label)); ?>',
-<?php endforeach; ?>
-		Tab: 'expand_abbreviation_with_tab',
-		Enter: 'insert_formatted_line_break_only'
-	};
-<?php endif; ?>
+	setTimeout(function() {
+		$('textarea:not(#wp_mce_fullscreen)').each(function() {
+			var $textarea = $(this),
+				file = $textarea.closest('form').find('input[name="file"]').val(),
+				mode = $textarea.attr('data-cm-mode'),
+				maxWidth = $textarea.attr('data-cm-max-width'),
+				minHeight = $textarea.attr('data-cm-min-height');
 
-	$(function() {
-		setTimeout(function() {
-			$('textarea:not(#wp_mce_fullscreen)').each(function() {
-				var file = $(this).closest('form').find('input[name="file"]').val();
-				$(this).codeMirror($.extend({}, options, {
-					mode: mimeTypes[file ? file.split('.').pop() : 'html']
-				}));
-			});
+			$textarea.codeMirror($.extend({}, options, {
+				mode: mode || mimeTypes[file ? file.split('.').pop() : 'html']
+			}));
 
-			wp_emmet.adaptCodeMirror();
-		}, 0);
-	});
-}(jQuery);
+			if (maxWidth) {
+				$($textarea.codeMirrorEditor().display.wrapper).css({maxWidth: maxWidth});
+			}
+
+			if (minHeight) {
+				$($textarea.codeMirrorEditor().display.scroller).css({minHeight: minHeight});
+			}
+		});
+
+		wp_emmet.adaptCodeMirror();
+	}, 0);
+});
 </script>
